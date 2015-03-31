57ad6c7745b1010a8de0d62a85903c7c8b4544e0
Anton-Volkov <stehnik7@gmai.com>
2015-04-01 01:59:09 +0300

    Add: action Map (that sends list with all Points to view Map), view Map ( that shows map, markers to all Points and implements simple search on map), link to Map on Layout.

--- BadRoads/BadRoads/Controllers/HomeController.cs
+++ BadRoads/BadRoads/Controllers/HomeController.cs
@@ -4,13 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BadRoads.Models;
 
 namespace BadRoads.Controllers
 {
-
     [Culture]
     public class HomeController : Controller
     {
+
+        BadroadsDataContext db = new BadroadsDataContext();      // объект модели
+
         public ActionResult Index()
         {
             return View();
@@ -53,6 +56,25 @@ namespace BadRoads.Controllers
             }
             Response.Cookies.Add(cookie);
             return Redirect(returnUrl);
+        }
+
+        public ActionResult Map()   // отображение основной карты со всеми сохраненными точками
+        {
+            //List<Point> listPoints = db.Points.ToList<Point>();                список всех точек в базе
+            List<Point> listPoints = new List<Point>();
+            for(int x = 0; x<100;x++)                              // заглушка. чтобы наполнить список с точками, которых пока нет в базе
+            {
+                double latitude = 48.459015 + (x * 0.00045);
+                double longitude = 35.042302 + (x * 0.00045);
+                string adress = String.Format("Проблема на улице " + x);
+                UserProfile u = new UserProfile();
+                GeoData g = new GeoData(latitude, longitude);
+                g.FullAddress = adress;
+                Point p = new Point(u);
+                p.GeoData = g;
+                listPoints.Add(p);
+            }
+            return View(listPoints);
         }
     }
 }
