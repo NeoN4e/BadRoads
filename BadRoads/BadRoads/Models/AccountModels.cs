448e2da3cd0374b0e9bb500e8cfac30845dae75b
Vasya Pypkin <vasy@ukr.net>
2015-03-31 09:34:13 +0300

    GIT IGNORE

--- BadRoads/BadRoads/Models/AccountModels.cs
+++ BadRoads/BadRoads/Models/AccountModels.cs
@@ -77,6 +77,10 @@ namespace BadRoads.Models
         [Display(Name = "User name")]
         public string UserName { get; set; }
 
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
