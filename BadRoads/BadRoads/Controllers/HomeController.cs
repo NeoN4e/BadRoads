78c95b65a4946e5db85ce279f4f05e3bb19ef15a
Ukron <dima.maliy@gmail.com>
2015-04-01 20:26:43 +0300

    - Move Point Info From HomeController to PointController - перенёс "BadroadsDataContext db" из экшена в контроллер

--- BadRoads/BadRoads/Controllers/HomeController.cs
+++ BadRoads/BadRoads/Controllers/HomeController.cs
@@ -72,30 +72,5 @@ namespace BadRoads.Controllers
             }
             return View(listPoints);
         }
-
-        public ActionResult PointInfo(int id)
-        {
-            //получение необходимой "точки" и передаём её во "View"
-            Point p = (from entry in db.Points where entry.ID == id select entry).Single();
-            return View(p);
-
-            //// заглушка. чтобы наполнить список с точками, которых пока нет в базе
-            //List<Point> listPoints = new List<Point>();
-            //for (int x = 0; x < 100; x++)                              
-            //{
-            //    double latitude = 48.459015 + (x * 0.00045);
-            //    double longitude = 35.042302 + (x * 0.00045);
-            //    string adress = String.Format("Проблема на улице " + x);
-            //    UserProfile u = new UserProfile();
-            //    GeoData g = new GeoData(latitude, longitude);
-            //    g.FullAddress = adress;
-            //    Point p = new Point(u);
-            //    p.GeoData = g;
-            //    listPoints.Add(p);
-            //}
-            //// 
-            //Point pnt = listPoints[0];
-            //return View(pnt);
-        }
     }
 }
