a5f3c1390aa051e0ec27b44dd6de950ee212997a
Don <trade400@gmail.com>
2015-04-01 19:02:13 +0300

    SimpleMembership filter added, Logout timeout changed on 30 minutes

--- BadRoads/BadRoads/App_Start/FilterConfig.cs
+++ BadRoads/BadRoads/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using BadRoads.Filters;
+using System.Web;
 using System.Web.Mvc;
 
 namespace BadRoads
@@ -8,6 +9,7 @@ namespace BadRoads
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new InitializeSimpleMembershipAttribute());
         }
     }
 }
