6ebc3ee003e6f71197484aac562d55d3cf941d66
unknown <dev@340-2-4.training.workrocks.ua>
2015-04-01 19:19:58 +0300

    Добавил PointController и записал в него Экшн своей задачи. (Добавление точки)

---
+++ BadRoads/BadRoads/Controllers/PointController.cs
@@ -0,0 +1,28 @@
+﻿using BadRoads.Filters;
+using BadRoads.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BadRoads.Controllers
+{
+    [Culture]
+    public class PointController : Controller
+    {        
+        [HttpPost]
+        public ActionResult Create(Point Pnt)
+        {
+            BadroadsDataContext db = new BadroadsDataContext();      // объект модели
+
+            if (ModelState.IsValid)
+            {
+                db.Points.Add(Pnt);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Pnt);
+        }
+    }
+}
