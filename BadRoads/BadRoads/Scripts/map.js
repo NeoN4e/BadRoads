70bc6843c487b3c7d69912f73ed291fcebcac0ff
Vasya Pypkin <vasy@ukr.net>
2015-04-02 13:35:49 +0300

    Поиск и позиционирование на карте

--- BadRoads/BadRoads/Scripts/map.js
+++ BadRoads/BadRoads/Scripts/map.js
@@ -4,6 +4,8 @@ var map;                                                  // карта
 var geocoder;                                             // объект класса Geocoder
 var markers = new Array();
 
+var searchMarker; // Маркер поиска улицы
+
 function SetPoints() {                                     // метод проставления всех точек на карте
     var masPoints = document.getElementsByClassName("points");   // получаем координаты точек с html
     var imageMarker = "../../Images/newmarkersmall.png";              // картинка для маркеров
@@ -35,12 +37,23 @@ function Initialize() {
     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);           // создание карты
     SetPoints();                                                                            // вызываем метод проставления всех точек на карте
     var markerClusterer = new MarkerClusterer(map, markers,                                 // создание объекта MarkerClusterer для группировки маркеров на карте
-{                                                                                               // настройки группировки
-    maxZoom: 13,
-    gridSize: 50,
-    styles: null
-});
+        {                                                                                               // настройки группировки
+            maxZoom: 13,
+            gridSize: 50,
+            styles: null
+        });
+
+    //Инициализация маркера поиска без него совсем плохо
+    //02 04 2015 Коноваленко А.В.
+    searchMarker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+    });
+    google.maps.event.addListener(searchMarker, 'click', function () {   // подписываем маркер на событие click
+        alert("GO TO Create point action");                              //  здесь будет вызываться экшен с подробным отображением точки, по которой кликнули. Передаем туда ID точки
+    });
 }
+
 function CodeAddress() {                                                                    // центрирование карты по адресу введенному в строке поиска
     geocoder.geocode({ 'address': $('#searchAdress').val() }, function (results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
@@ -50,3 +63,41 @@ function CodeAddress() {                                                        
         }
     });
 }
+
+//Автокомплит плюс позиционирование
+//02 04 2015 Коноваленко А.В.
+$(function() {
+    $("#searchAdress").autocomplete({
+        //Определяем значение для адреса при геокодировании
+        source: function (request, response) {
+            var geocoderRequest = {
+            //    'address': request.term,
+                'componentRestrictions': {
+                    'country': 'ua',
+                    'locality': 'Днепропетровск',
+                    'route': request.term
+                },
+            };
+
+            geocoder.geocode(geocoderRequest, function (results, status) {
+                response($.map(results, function(item) {
+                    return {
+                        FullAddress: item.formatted_address,
+                        value: item.formatted_address,
+                        latitude: item.geometry.location.lat(),
+                        longitude: item.geometry.location.lng()
+                    }
+                }));
+            })
+        },
+
+        //Выполняется при выборе конкретного адреса
+        select: function(event, ui) {
+            var location = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
+            map.setCenter(location);
+
+            searchMarker.setPosition(location);
+            searchMarker.setTitle(ui.item.FullAddress);
+        }
+    });
+})
