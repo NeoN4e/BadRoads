11e9ead2bc5102af47a0f11547bddb13b013453b
Anton-Volkov <stehnik7@gmai.com>
2015-04-06 14:52:59 +0300

    Fix path from Add to PoinInfo and some bags with script for Map and Add.

--- BadRoads/BadRoads/Scripts/map.js
+++ BadRoads/BadRoads/Scripts/map.js
@@ -27,22 +27,31 @@ function AlreadySetLocation() {                                          // фу
 
 function SetPoints() {                                     // метод проставления всех точек на карте
     var masPoints = document.getElementsByClassName("points");   // получаем координаты точек с html
+    if (masPoints.length > 0) {
+        for (var x = 0; x < masPoints.length; x++) {
+            var la = $(masPoints[x]).data('latitude');
+            la = la.replace(",", ".");
+            var ln = $(masPoints[x]).data('longitude');
+            ln = ln.replace(",", ".");
+            markers[x] = new google.maps.Marker({                    // создаем маркер
+                position: new google.maps.LatLng(la, ln),
+                map: map,
+                icon: imageMarker,
+                title: $(masPoints[x]).data('adress')
+            });
+            markers[x].idPoint = $(masPoints[x]).data('id');                        // присваиваем маркеру свойство с ID точки
+            google.maps.event.addListener(markers[x], 'click', function () {       // подписываем маркер на событие click
+                window.location.assign("../../Point/PointInfo/" + this.idPoint);   // переход на экшен подробного отображения точки
+            });
+        }
 
-    for (var x = 0; x < masPoints.length; x++) {
-        var la = $(masPoints[x]).data('latitude');
-        la = la.replace(",", ".");
-        var ln = $(masPoints[x]).data('longitude');
-        ln = ln.replace(",", ".");
-        markers[x] = new google.maps.Marker({                    // создаем маркер
-            position: new google.maps.LatLng(la, ln),
-            map: map,
-            icon: imageMarker,
-            title: $(masPoints[x]).data('adress')
+        var markerClusterer = new MarkerClusterer(map, markers,                                 // создание объекта MarkerClusterer для группировки маркеров на карте
+        {                                                                                               // настройки группировки
+            maxZoom: 13,
+            gridSize: 50,
+            styles: null
         });
-        markers[x].idPoint = $(masPoints[x]).data('id');                        // присваиваем маркеру свойство с ID точки
-        google.maps.event.addListener(markers[x], 'click', function () {       // подписываем маркер на событие click
-            window.location.assign("../../Point/PointInfo/"+this.idPoint);   // переход на экшен подробного отображения точки
-        });
+
     }
 
 }
@@ -57,12 +66,7 @@ function Initialize() {
     };
     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);           // создание карты
     SetPoints();                                                                            // вызываем метод проставления всех точек на карте
-    var markerClusterer = new MarkerClusterer(map, markers,                                 // создание объекта MarkerClusterer для группировки маркеров на карте
-        {                                                                                               // настройки группировки
-            maxZoom: 13,
-            gridSize: 50,
-            styles: null
-        });
+
 
     //Инициализация маркера поиска без него совсем плохо
     //02 04 2015 Коноваленко А.В.
